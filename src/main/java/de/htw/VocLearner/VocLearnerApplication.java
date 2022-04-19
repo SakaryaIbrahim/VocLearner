620e6313bcbbf9f2073f1afd038670ecb3e25904
SakaryaIbrahim <ibrahim.sakarya2002@gmail.com>
2022-04-19 22:52:40 +0200

    init

---
+++ src/main/java/de/htw/VocLearner/VocLearnerApplication.java
@@ -0,0 +1,13 @@
+package de.htw.VocLearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocLearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocLearnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw/VocLearner/VocLearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.VocLearner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocLearnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
